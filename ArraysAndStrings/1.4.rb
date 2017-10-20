b3f0d1c46bfde6ee3034ec3d28e0e1e69977be47
illiteratelinds <lindsay.padian@gmail.com>
2017-10-19 23:29:15 -0400

    question 4 done

--- ArraysAndStrings/1.4.rb
+++ ArraysAndStrings/1.4.rb
@@ -1,3 +1,16 @@
 # 1.4 Write a method to replace all spaces in a string with'%20'. You may assume that the string has sufficient space at the end of the string to hold the additional characters, and that you are given the "true" length of the string. (Note: if imple- menting in Java, please use a character array so that you can perform this opera- tion in place.)
 # EXAMPLE
 # Input: "Mr John Smith Output: "Mr%20Dohn%20Smith"
+
+
+def replace_spaces(string)
+  char_array = string.split('')
+  char_array.collect! do |character|
+    (character == ' ') ? '%20' : character
+  end
+  char_array = char_array.join
+  return char_array
+end
+
+string = "Mr John Smith"
+replace_spaces(string)
